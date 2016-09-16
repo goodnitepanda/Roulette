81bc8984f30ca03b46212727acaf77dde3aeb986
whamm <whamann0@gmail.com>
2016-09-16 11:00:55 -0700

    getting data returned from google and error handling put in

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -1,33 +1,30 @@
 <?php
   require 'helperfunctions\callAPI.php';
 
-  class PagesController 
+  class PagesController
   {
 
-    public function home() 
+    public function home()
       {
         $first_name = 'Jon';
         $last_name  = 'Snow';
         require_once('views/pages/home.php');
       }
-    public function error() 
+    public function error()
       {
 
         require_once('views/pages/error.php');
       }
     public function getRestaurant()
       {
-        $apiendpoint = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";    
-        $APIkey = "AIzaSyA30yhaBrGHSuhrdyBsy9wuLIDoYO6qv0s";
         $startLat = $_GET['lat'];
         $startLon = $_GET['lon'];
+        $apiendpoint = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"."location=".$startLat.",".$startLon."&radius=16093.4"."&type=restaurant"."&key="."AIzaSyA30yhaBrGHSuhrdyBsy9wuLIDoYO6qv0s";
         $helper = new helperfunctions;
 
-        $constructedURL = $apiendpoint."location=".$startLat.",".$startLon."&radius=16093.4"."&type=restaurant"."&key=".$APIkey;
-        $resturantData = $helper->CallAPI($constructedURL);
+        $resturantData = $helper->CallAPI($apiendpoint);
 
-        //$testvar = 'chhhheeeeese';
         echo json_encode($resturantData);
       }
-  }    
+  }
 ?>
--- helperfunctions/callAPI.php
+++ helperfunctions/callAPI.php
@@ -3,41 +3,29 @@ class helperfunctions {
 
     public function CallAPI($url)
     {
+      try {
         $curl = curl_init();
 
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($curl, CURLOPT_URL, urlencode($url));
-
+        curl_setopt($curl, CURLOPT_URL, $url);
 
         $result = curl_exec($curl);
 
+        if (FALSE === $result)
+        {
+          throw new Exception(curl_error($curl), curl_errno($curl));
+        }
         curl_close($curl);
-
-        // switch ($method)
-        // {
-        //     case "GET":
-        //         curl_setopt($curl, CURLOPT_POST, 1);
-
-        //         if ($data)
-        //             curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        //         break;
-        //     case "PUT":
-        //         curl_setopt($curl, CURLOPT_PUT, 1);
-        //         break;
-        //     // default:
-        //     //     if ($data)
-        //     //         $url = sprintf("%s?%s", $url, http_build_query($data));
-        // }
-
-        // Optional Authentication:
-        // curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        // curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-
-        // curl_setopt($curl, CURLOPT_URL, $url);
-        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
+      }
+       catch (Exception $e)
+       {
+        trigger_error(sprintf(
+        'Curl failed with error #%d: %s',
+        $e->getCode(), $e->getMessage()),
+        E_USER_ERROR);
+      }
         return $result;
     }
 }
